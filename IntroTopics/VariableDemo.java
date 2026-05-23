4c460bbd4bf731324c06d3bb966f4aff64272aea
Nayan Singhal <nayansinghal393@gmail.com>
2026-05-23 09:14:35 +0530

    Introduction to JAVA

---
+++ IntroTopics/VariableDemo.java
@@ -0,0 +1,16 @@
+package IntroTopics;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        int speedLimit = 60;
+        boolean flag = true;
+
+        System.out.println("speedLimit : " + speedLimit);
+        System.out.println("flag : " + flag);
+
+        /**
+         * Primitive Data Types - byte, short, int, long, float, double, char, boolean
+         * Non-Primitive Data Types - String, Array, Class, Object
+         */
+    }
+}
